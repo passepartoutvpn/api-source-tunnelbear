419690e6c23fe01cc069ec1160cfc7245430d987
Davide De Rosa <keeshux@gmail.com>
2019-03-08 10:51:38 +0100

    Fix sed range incompatible with Linux version
    Fixes #1

--- scripts/update-servers.sh
+++ scripts/update-servers.sh
@@ -9,6 +9,6 @@ mv tmp/openvpn/CACertificate.crt certs/ca.pem
 openssl x509 -in tmp/openvpn/UserCertificate.crt -out certs/client.pem # strip text header
 mv tmp/openvpn/PrivateKey.key certs/client.key
 
-grep ^remote tmp/openvpn/*.ovpn | sed -E "s/^.*TunnelBear\ ([^\.]+)\.ovpn:remote ([[A-z0-9\.]+).*443.*$/\1,\2/" >template/servers.csv
+grep ^remote tmp/openvpn/*.ovpn | sed -E "s/^.*TunnelBear\ ([^\.]+)\.ovpn:remote ([[A-Za-z0-9\.]+).*443.*$/\1,\2/" >template/servers.csv
 
 rm -rf tmp
